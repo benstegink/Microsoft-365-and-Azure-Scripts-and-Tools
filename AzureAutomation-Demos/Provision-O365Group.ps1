cb9054a21be5b9824ae30ad24fdb21183a21ca6d
Ben <bstegink@intelligink.com>
2018-12-03 17:15:23 -0500

    Updated to use New-PnPUnifiedGroup

--- AzureAutomation-Demos/Provision-O365Group.ps1
+++ AzureAutomation-Demos/Provision-O365Group.ps1
@@ -12,7 +12,13 @@ Param(
     [Parameter (Mandatory=$false)]
     [String] $createTeam,
     [Parameter (Mandatory=$false)]
-    [String] $creator
+    [String] $creator,
+    [Parameter (Mandatory=$false)]
+    [String] $friendlyAlias,
+    [Parameter (Mandatory=$true)]
+    [String] $addressbookvisible,
+    [Parameter (Mandatory=$true)]
+    [String] $externalemail
 )
 
 $UserCredential = Get-AutomationPSCredential -Name 'Provisioning'
@@ -22,48 +28,19 @@ Import-Module (Import-PSSession -Session $Session -AllowClobber -DisableNameChec
 $username = $UserCredential.UserName
 
 if($createTeam -eq "False"){
-   
-    $password = $UserCredential.GetNetworkCredential().Password
-    $client_id = "";
-    $client_secret = "";
-    $tenant_id = "";
-    $resource = "https://graph.microsoft.com";
-    # grant_type = password
-    $authority = "https://login.microsoftonline.com/$tenant_id";
-    $tokenEndpointUri = "$authority/oauth2/token"
-    $content = "grant_type=password&username=$username&password=$password&client_id=$client_id&client_secret=$client_secret&resource=$resource";
-    $response = Invoke-WebRequest -Uri $tokenEndpointUri -Body $content -Method Post -UseBasicParsing
-    $responseBody = $response.Content | ConvertFrom-JSON
-    #$responseBody
-    #$responseBody.access_token
-
-
-    $access_token = $responseBody.access_token
-
-    # GET https://graph.microsoft.io/en-us/docs/api-reference/v1.0/api/group_list
-    $body = Invoke-RestMethod -Uri "https://graph.microsoft.com/v1.0/groups" -Headers @{"Authorization" = "Bearer $access_token"}
-    #$body | ConvertTo-JSON
-
-    # POST - this creates groups https://graph.microsoft.io/en-us/docs/api-reference/v1.0/api/group_post_groups
-    $body = @{"displayName"=$groupDisplayName; "mailEnabled"=$false; "groupTypes"=@("Unified"); "securityEnabled"=$false; "mailNickname"=$groupAlias;} | ConvertTo-Json 
-    $body = Invoke-RestMethod `
-        -Uri "https://graph.microsoft.com/v1.0/groups" `
-        -Headers @{"Authorization" = "Bearer $access_token"} `
-        -Body $body `
-        -ContentType "application/json" `
-        -Method POST
-    #$body | ConvertTo-JSON
-
-    while($group -eq $null){
-        $group = Get-UnifiedGroup -Identity $groupAlias -ErrorAction SilentlyContinue
-
-        Start-Sleep -Seconds 60
-        Write-Output "Waiting for Group to Create"
-    }
+    Connect-PnPOnline -AppId db22d1ee-2d8f-4368-8b53-291cd317477b -AppSecret $secret -AADDomain 'intelligink.onmicrosoft.com'
+    New-PnPUnifiedGroup -DisplayName $groupDisplayName -Description $groupDisplayName -Owners $groupOnwer -MailNickname $groupAlias
 }
 else{
     Connect-MicrosoftTeams -Credential $UserCredential
     $groupId = New-Team -DisplayName $groupDisplayName -Alias $groupAlias
+}
+
+while($group -eq $null){
+    $group = Get-UnifiedGroup -Identity $groupAlias -ErrorAction SilentlyContinue
+
+    Start-Sleep -Seconds 60
+    Write-Output "Waiting for Group to Create"
 }
 
 if($groupAccessType -eq "Private"){
@@ -71,14 +48,47 @@ if($groupAccessType -eq "Private"){
 }
 
 Add-UnifiedGroupLinks -Identity $groupAlias -Links $creator -LinkType Members
+Start-Sleep -Seconds 20
 Add-UnifiedGroupLinks -Identity $groupAlias -Links $creator -LinkType Owners
 
 Add-UnifiedGroupLinks -Identity $groupAlias -Links $groupOwner -LinkType Members
+Start-Sleep -Seconds 20
 Add-UnifiedGroupLinks -Identity $groupAlias -Links $groupOwner -LinkType Owners
 Remove-UnifiedGroupLinks -Identity $groupAlias -Links $username -LinkType Owners -Confirm:$false
 Remove-UnifiedGroupLinks -Identity $groupAlias -Links $username -LinkType Members -Confirm:$false
 
-if($groupSecondaryOwner -ne $null){
+if($groupSecondaryOwner -ne $null -and $groupSecondaryOwner -ne ""){
     Add-UnifiedGroupLinks -Identity $groupAlias -Links $groupSecondaryOwner -LinkType Members
+    Start-Sleep -Seconds 20
     Add-UnifiedGroupLinks -Identity $groupAlias -Links $groupSecondaryOwner -LinkType Owners
+}
+
+if($friendlyAlias -ne ""){
+    $rec = Get-Recipient $friendlyAlias -ErrorAction SilentlyContinue
+    if($rec -eq $null){
+        Set-UnifiedGroup -Identity $groupAlias -Alias $friendlyAlias
+        $aptarEmail = ("smtp:" + $friendlyAlias + "@aptar.com")
+        $aptarMSEmail = ("smtp:" + $friendlyAlias + "@aptar.onmicrosoft.com")
+        $primarySMTP = ($friendlyAlias + "@aptar.onmicrosoft.com")
+        Write-Output "New Email Addresses: $aptarEmail, $aptarMSEmail with the primary address of $primarySMTP"
+        Set-UnifiedGroup -Identity $groupDisplayName -EmailAddresses @{Add=$aptarEmail,$aptarMSEmail}
+        Set-UnifiedGroup -Identity $groupDisplayName -PrimarySmtpAddress $primarySMTP
+    }
+    else{
+        Write-Output "the alias $friendlyAlias already exists"
+    }
+}
+
+if($externalemail){
+    Set-UnifiedGroup -Identity $friendlyAlias -RequireSenderAuthenticationEnabled $false
+}
+else{
+    Set-UnifiedGroup -Identity $friendlyAlias -RequireSenderAuthenticationEnabled $true
+}
+
+if($addressbookvisible){
+    Set-UnifiedGroup -Identity $friendlyAlias -HiddenFromAddressListsEnabled $false
+}
+else{
+    Set-UnifiedGroup -Identity $friendlyAlias -HiddenFromAddressListsEnabled $true
 }
